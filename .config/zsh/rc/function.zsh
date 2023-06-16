1bac515733700348cc78a3bfd9f65faee53eb219
yoohya <20457167+yoohya@users.noreply.github.com>
2023-06-17 04:41:35 +0900

    fix j

--- .config/zsh/rc/function.zsh
+++ .config/zsh/rc/function.zsh
@@ -17,17 +17,9 @@ j() {
   if command -v exa &> /dev/null; then
     preview_cmd="exa -l {2}"
   fi
-
   if [[ $# -eq 0 ]]; then
-    cd "$(autojump -s | sort -k1gr | awk -F : '$1 ~ /[0-9]/ && $2 ~ /^\s*\// {print $1 $2}' | fzf --height 40% --reverse --inline-info --preview "$preview_cmd" --preview-window down:50% | cut -d$'\t' -f2- | sed 's/^\s*//')"
+    cd "$(autojump -s | sort -k1gr | fzf --height 40% --reverse --inline-info --preview "$preview_cmd" --preview-window down:50% | cut -d$'\t' -f2- | sed 's/^\s*//')"
   else
     cd $(autojump $@)
   fi
-}
-
-# like normal z when used with arguments but displays an fzf prompt when used without
-unalias z 2> /dev/null
-z() {
-  [ $# -gt 0 ] && _z "$*" && return
-  cd "$(_z -l 2>&1 | fzf --height 40% --nth 2.. --reverse --inline-info +s --tac --query "${*##-* }" | sed 's/^[0-9,.]* *//')"
 }
